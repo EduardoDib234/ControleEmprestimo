b8ff092148dcd6f9beed701331d6a523108729d2
jhonatan <[jhonaatan81@gmail.com](mailto:jhonaatan81@gmail.com)>
2024-11-11 19:29:32 -0200

    Criaçao das classes DAO, conexaoDB e mensagem

--- src/main/java/com/mycompany/controleemprestimo/dao/ConexaoDB.java
+++ src/main/java/com/mycompany/controleemprestimo/dao/ConexaoDB.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.controleemprestimo.model;
+package com.mycompany.controleemprestimo.dao;
 
 /**
  *
  * @author Administrator
  */
-public class Ferramentas {
-    private int id;
-    private String nome;
-    private String marca;
-    private double custo;
-    private boolean status;
+public class ConexaoDB {
     
 }
---
+++ src/main/java/com/mycompany/controleemprestimo/model/Ferramenta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controleemprestimo.model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ferramenta {
+    private int id;
+    private String nome;
+    private String marca;
+    private double custo;
+    private boolean status;
+    
+}
