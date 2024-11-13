378e2227cba6a4150fc112f9af108a593cf6ff3f
jhonatan <[jhonaatan81@gmail.com](mailto:jhonaatan81@gmail.com)>
2024-11-13 16:09:07 -0200

    Banco de dados finalizado (amigos, ferramenta, emprestimo e ferraementas emprestadas)

---
+++ db.sql
@@ -0,0 +1,47 @@
+-- Criar o database se não existir
+CREATE DATABASE IF NOT EXISTS bd_controle_emprestimo;
+
+-- Entrar no database
+USE bd_controle_emprestimo;
+
+-- Remover as tabelas caso já existam
+DROP TABLE IF EXISTS ferramentas_emprestadas;
+DROP TABLE IF EXISTS emprestimos;
+DROP TABLE IF EXISTS amigos;
+DROP TABLE IF EXISTS ferramentas;
+
+-- Criar a tabela de ferramentas
+CREATE TABLE IF NOT EXISTS ferramentas (
+    id_ferramenta INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    marca VARCHAR(255),
+    custo_aquisicao DECIMAL(10, 2),
+    status BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- Criar a tabela de amigos
+CREATE TABLE IF NOT EXISTS amigos (
+    id_amigo INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    telefone VARCHAR(20)
+);
+
+-- Criar a tabela de empréstimos
+CREATE TABLE IF NOT EXISTS emprestimos (
+    id_emprestimo INT AUTO_INCREMENT PRIMARY KEY,
+    id_amigo INT,
+    id_ferramenta INT,
+    data_emprestimo DATE,
+    data_devolucao DATE,
+    status INT NOT NULL,
+    FOREIGN KEY (id_ferramenta) REFERENCES ferramentas(id_ferramenta),
+    FOREIGN KEY (id_amigo) REFERENCES amigos(id_amigo)
+);
+
+-- Criar a tabela de ferramentas emprestadas
+CREATE TABLE IF NOT EXISTS ferramentas_emprestadas (
+    id_emprestimo INT NOT NULL,
+    id_ferramenta INT NOT NULL,
+    FOREIGN KEY (id_emprestimo) REFERENCES emprestimos(id_emprestimo),
+    FOREIGN KEY (id_ferramenta) REFERENCES ferramentas(id_ferramenta)
+);
