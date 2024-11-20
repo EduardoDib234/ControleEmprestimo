87c1b53fd11f75ac35f5fe84d86d7d82d21e09a8
C0mrad078 <[jhonaatan81@gmail.com](mailto:jhonaatan81@gmail.com)>
2024-11-19 23:46:05 -0200

    chore: renomeia arquivo do banco de dados para padronização.

---
+++ banco.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS bd_controle_emprestimo;
+
+USE bd_controle_emprestimo;
+
+DROP TABLE IF EXISTS ferramentas_emprestadas;
+DROP TABLE IF EXISTS emprestimos;
+DROP TABLE IF EXISTS amigos;
+DROP TABLE IF EXISTS ferramentas;
+
+CREATE TABLE IF NOT EXISTS ferramentas (
+    id_ferramenta INT AUTO_INCREMENT PRIMARY KEY,
+    nome_ferramentas VARCHAR(255),
+    marca VARCHAR(255),
+    custo DECIMAL(10, 2),
+    status VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS amigos (
+    id_amigo INT AUTO_INCREMENT PRIMARY KEY,
+    nome_amigo VARCHAR(255),
+    telefone VARCHAR(20),
+    score VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS emprestimos (
+    id_emprestimo INT AUTO_INCREMENT PRIMARY KEY,
+    id_amigo INT,
+    id_ferramenta INT,
+    data_emprestimo DATE,
+    data_devolucao DATE,
+    status INT NOT NULL,
+    FOREIGN KEY (id_ferramenta) REFERENCES ferramentas(id_ferramenta),
+    FOREIGN KEY (id_amigo) REFERENCES amigos(id_amigo)
+);
+
+CREATE TABLE IF NOT EXISTS ferramentas_emprestadas (
+    id_emprestimo INT NOT NULL,
+    id_ferramenta INT NOT NULL,
+    FOREIGN KEY (id_emprestimo) REFERENCES emprestimos(id_emprestimo),
+    FOREIGN KEY (id_ferramenta) REFERENCES ferramentas(id_ferramenta)
+);
